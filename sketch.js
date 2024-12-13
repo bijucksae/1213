eee7b013e798c8aa17d687a20d773f62f669db35
bijucksae <jiwonmin000@gmail.com>
2024-12-13 16:00:55 +0900

    노이즈 기반 각도 계산

--- sketch.js
+++ sketch.js
@@ -16,6 +16,7 @@ function draw() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       let index = x + y * cols;
+      let angle = noise(x * 0.1, y * 0.1, millis() * 0.0001) * TWO_PI * 2;
     }
   }  
 }
