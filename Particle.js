520500b216b308faa166fe35f211f4b16b7dcae6
bijucksae <jiwonmin000@gmail.com>
2024-12-13 16:06:52 +0900

    속도 제한 값 설정

--- Particle.js
+++ Particle.js
@@ -3,5 +3,7 @@ class Particle {
     this.position = createVector(random(width), random(height));
     this.velocity = createVector(0, 0);
     this.acceleration = createVector(0, 0);
+    this.maxspeed = 4;
+    this.maxforce = 0.1;
   }  
 }
