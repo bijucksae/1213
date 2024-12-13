5f9b1eea387a6db9f89f86540d641914d2ec60f3
bijucksae <jiwonmin000@gmail.com>
2024-12-13 16:06:28 +0900

    생성자에 acceleration 초기화

--- Particle.js
+++ Particle.js
@@ -2,5 +2,6 @@ class Particle {
   constructor() {
     this.position = createVector(random(width), random(height));
     this.velocity = createVector(0, 0);
+    this.acceleration = createVector(0, 0);
   }  
 }
