4e588a6988d0b48906dd6b76aad05937cacea0d0
bijucksae <jiwonmin000@gmail.com>
2024-12-16 18:22:21 +0900

    점으로 표현

--- IndependentParticle.js
+++ IndependentParticle.js
@@ -15,8 +15,7 @@ class IndependentParticle extends Particle {
   show() {
     stroke(255, 100, 100, 150);
     strokeWeight(3);
-    line(this.previousPosition.x, this.previousPosition.y, this.position.x, this.position.y);
-    this.previousPosition = this.position.copy();
+    point(this.position.x, this.position.y);
   }
 
   edges() {
