ec0e4d76c15c066abdb26f790a1a07b86b5a1f95
bijucksae <jiwonmin000@gmail.com>
2024-12-16 18:08:09 +0900

    edges 추가

--- IndependentParticle.js
+++ IndependentParticle.js
@@ -17,4 +17,11 @@ class IndependentParticle extends Particle {
     line(this.previousPosition.x, this.previousPosition.y, this.position.x, this.position.y);
     this.previousPosition = this.position.copy();
   }
+
+  edges() {
+    if (this.position.x > width) this.position.x = width;
+    if (this.position.x < 0) this.position.x = 0;
+    if (this.position.y > height) this.position.y = height;
+    if (this.position.y < 0) this.position.y = 0;
+  }
 }
