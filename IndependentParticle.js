2f9febcb2b0667550dd4d1320b06749b3ce8d7ea
bijucksae <jiwonmin000@gmail.com>
2024-12-16 17:40:36 +0900

    이전 위치에서 현재 위치까지 선 그리기

--- IndependentParticle.js
+++ IndependentParticle.js
@@ -2,5 +2,6 @@ class IndependentParticle extends Particle {
   show() {
     stroke(255, 100, 100, 150);
     strokeWeight(3);
+    line(this.previousPosition.x, this.previousPosition.y, this.position.x, this.position.y);
   }
 }
